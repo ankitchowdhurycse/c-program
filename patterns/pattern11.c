b54d11245620ebebde4c323b7e66eca3298a9d41
Ankit Chowdhury <ankitchowdhury.cse2@gmail.com>
2021-11-03 21:25:39 +0530

    Added pattern11.c

---
+++ patterns/pattern11.c
@@ -0,0 +1,26 @@
+/* This program will print the following pattern:
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+*/
+
+#include <stdio.h>
+
+int main() {
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= rows+1; col++)
+        {
+            if (col <= row)
+                printf(" ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+}

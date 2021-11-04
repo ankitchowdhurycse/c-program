eaa147199ef93a7baf171432f8c5d58db3bd4c76
Ankit Chowdhury <ankitchowdhury.cse2@gmail.com>
2021-11-04 11:33:06 +0530

    Added pattern12.c

---
+++ patterns/pattern12.c
@@ -0,0 +1,43 @@
+/* This program will print the following pattern:
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+    * 
+   * *
+  * * *
+ * * * *
+* * * * *
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= rows + 1; col++)
+        {
+            if (col <= row)
+                printf(" ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= rows; col++)
+        {
+            if (col <= (rows - row))
+                printf(" ");
+            else
+                printf(" *");
+        }
+        printf("\n");
+    }
+}

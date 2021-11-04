95476b1861bea255aba33bba780287849b772059
Ankit Chowdhury <ankitchowdhury.cse2@gmail.com>
2021-11-04 11:04:52 +0530

    Edit pattern9.c

--- patterns/pattern9.c
+++ patterns/pattern9.c
@@ -10,39 +10,18 @@
 
 int main()
 {
-    int rows, row, col, k = 0;
+    int rows, row, col;
     printf("Enter number of rows: ");
     scanf("%d", &rows);
-    for (row = rows; row > 0; row--)
+    for (row = 1; row <= rows; row++)
     {
-        for (col = 1; col <= k; col++)
-            printf(" ");
-        for (col = 1; col <= row; col++)
-            printf("*");
-        for (col = 1; col <= row; col++)
-            printf("*");
+        for (col = 1; col <= 2*rows-row; col++)
+        {
+            if (col < row)
+                printf(" ");
+            else
+                printf("*");
+        }
         printf("\n");
-        k++;
     }
-
-    return 0;
 }
-
-// #include <stdio.h>
-
-// int main() {
-//     int rows;
-//     printf("Enter number of rows: ");
-//     scanf("%d", &rows);
-//     for (int row = 1; row <= rows; row++)
-//     {
-//         for (int col = 1; col <= rows+row-1; col++)
-//         {
-//             if (col < row)
-//                 printf(" ");
-//             else
-//                 printf("*");
-//         }
-//         printf("\n");
-//     }
-// }
